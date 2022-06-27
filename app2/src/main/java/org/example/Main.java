f9335b31f7761b00a45afe5b2ae545571fd20122
Suleyman <61500642+suleymanakay@users.noreply.github.com>
2022-06-27 19:39:10 +0300

    Add files via upload

---
+++ app2/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import org.example.Model.Store;
+
+public class Main {
+    public static void main(String[] args) {
+        Store patikaStore = new Store();
+        patikaStore.run();
+
+
+    }
+}
